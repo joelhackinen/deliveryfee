54ee58fd4784d717e7dda1441a801c190f3beb32
Joel <joel.hakkinen@gmail.com>
2023-01-23 22:38:26 +0200

    added date picker

--- src/components/DeliveryForm.tsx
+++ src/components/DeliveryForm.tsx
@@ -44,18 +44,25 @@ const DeliveryForm = (props: FormProps) => {
             <Form.Control type="number" value={distance} onChange={(event) => setDistance(event.target.value)} placeholder="distance"/>
           </FloatingLabel>
         </Form.Group>
-        <Form.Group as={Col}>
-          <Form.Range value={distance} min="1" max="15000" onChange={(event) => setDistance(event.target.value)}/>
+        <Form.Group as={Col} className="mb-3">
+          <Form.Range style={{ marginTop: 10 }} value={distance} min="1" max="15000" onChange={(event) => setDistance(event.target.value)}/>
         </Form.Group>
       </Row>
-      <FloatingLabel label="Amount of items" className="mb-3">
-        <Form.Control type="number" value={amount} onChange={(event) => setAmount(event.target.value)} placeholder="amount"/>
-      </FloatingLabel>
-      <Form.Group className="mb-3">
-        <Form.Check type="checkbox" label="Rush hour delivery?" onClick={() => setRushHour(!rushHour)}/>
-      </Form.Group>
+      <Row>
+        <Form.Group as={Col}>
+          <FloatingLabel label="Amount of items" className="mb-3">
+            <Form.Control type="number" value={amount} onChange={(event) => setAmount(event.target.value)} placeholder="amount"/>
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Control type="date" />
+          <Form.Group className="mb-3" >
+            <Form.Check type="checkbox" label="Rush hour (3 PM - 7 PM) delivery?" onClick={() => setRushHour(!rushHour)}/>
+          </Form.Group>
+        </Form.Group>
+      </Row>
       <Button variant="primary" type="submit" disabled={!fieldsOk}>
-        Submit
+        Calculate
       </Button>
     </Form>
   );
