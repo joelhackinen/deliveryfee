c9d091643a99aa0de27dae7b582f94f97d64dc16
Joel Häkkinen <joel.hakkinen@gmail.com>
2023-01-28 18:56:53 +0200

    added form validation

--- src/components/DeliveryForm.tsx
+++ src/components/DeliveryForm.tsx
@@ -1,5 +1,13 @@
 import { FormEvent, useState } from "react";
-import { Button, Form, FloatingLabel, Row, Col } from "react-bootstrap";
+
+import {
+  Button,
+  Form,
+  FloatingLabel,
+  Row,
+  Col,
+  Alert
+} from "react-bootstrap";
 
 import { FormProps } from "../types";
 
@@ -9,11 +17,18 @@ const DeliveryForm = (props: FormProps) => {
   const [amount, setAmount] = useState<string>("");
   const [date, setDate] = useState<string>("");
   const [time, setTime] = useState<string>("");
+  const [errs, setErrors] = useState<string[]>([]);
 
-  const fieldsOk = Number(cart) !== 0 && Number(distance) !== 0 && Number(amount) !== 0 && date !== "" && time !== "";
+  const fieldsFilled = cart !== "" && distance !== "" && amount !== "" && date !== "" && time !== "";
 
   const submit = (event: FormEvent) => {
     event.preventDefault();
+    const e = validateFields();
+    if (e.length !== 0) {
+      setErrors(e);
+      return;
+    }
+    setErrors([]);
     props.showFee({
       cart: Number(cart),
       distance: Number(distance),
@@ -31,67 +46,94 @@ const DeliveryForm = (props: FormProps) => {
     setTime("");
   };
 
+  const validateFields = (): string[] => {
+    const errs: string[] = []
+    if (isNaN(Number(cart)) || Number(cart) <= 0) {
+      errs.push("Cart Value has to be a nonzero positive number.");
+    }
+    if (isNaN(Number(distance)) || Number(distance) <= 0) {
+      errs.push("Distance has to be a nonzero positive number.");
+    }
+    if (isNaN(Number(amount)) || Number(amount) <= 0 || Number(amount) % 1 !== 0) {
+      errs.push("Amount has to be a nonzero, positive whole number.");
+    }
+    if (date === "" || date.split("-").length !== 3) {
+      errs.push("Missing or invalid delivery date.");
+    }
+    if (time === "" || time.split(":").length !== 2) {
+      errs.push("Missing or invalid delivery time.");
+    }
+    return errs;
+  }
+
   return (
-    <Form onSubmit={submit}>
-      <Form.Group>
-        <FloatingLabel label="Cart Value" className="mb-3">
-          <Form.Control
-            type="number"
-            value={cart}
-            onChange={(event) => setCart(event.target.value)}
-            placeholder="Cart Value"
-          />
-        </FloatingLabel>
-      </Form.Group>
-      <Form.Group>
-        <FloatingLabel label="Delivery Distance" className="mb-3">
-          <Form.Control
-            type="number"
-            value={distance}
-            onChange={(event) => setDistance(event.target.value)}
-            placeholder="Delivery Distance"
-          />
-        </FloatingLabel>
-      </Form.Group>
-      <Row className="mb-3">
-        <Form.Group as={Col} className="mb-3" md>
-          <FloatingLabel label="Amount of items">
+    <div>
+      {
+        errs.length === 0
+          ? null
+          : <Alert variant="warning">{errs.join("\n")}</Alert>
+      }
+      <Form onSubmit={submit}>
+        <Form.Group>
+          <FloatingLabel label="Cart Value" className="mb-3">
             <Form.Control
-              type="number"
-              value={amount}
-              onChange={(event) => setAmount(event.target.value)}
-              placeholder="Amount of items"
+              type="text"
+              value={cart}
+              onChange={(event) => setCart(event.target.value)}
+              placeholder="Cart Value"
             />
           </FloatingLabel>
         </Form.Group>
-        <Form.Group as={Col} className="mb-3" sm>
-          <FloatingLabel label="Delivery date" >
+        <Form.Group>
+          <FloatingLabel label="Delivery Distance" className="mb-3">
             <Form.Control
-              type="date"
-              value={date}
-              onChange={(event) => setDate(event.target.value)}
-              placeholder="Delivery date"
+              type="text"
+              value={distance}
+              onChange={(event) => setDistance(event.target.value)}
+              placeholder="Delivery Distance"
             />
           </FloatingLabel>
         </Form.Group>
-        <Form.Group as={Col} className="mb-3" sm>
-          <FloatingLabel label="Delivery time (UTC)" >
-            <Form.Control
-              type="time"
-              value={time}
-              onChange={(event) => setTime(event.target.value)}
-              placeholder="Delivery time (UTC)"
-            />
-          </FloatingLabel>
-        </Form.Group>
-      </Row>
-      <Button variant="primary" type="submit" disabled={!fieldsOk} style={{ marginRight: 10 }}>
-        Calculate
-      </Button>
-      <Button variant="outline-danger" onClick={resetFields}>
-        Reset
-      </Button>
-    </Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} className="mb-3" md>
+            <FloatingLabel label="Amount of items">
+              <Form.Control
+                type="text"
+                value={amount}
+                onChange={(event) => setAmount(event.target.value)}
+                placeholder="Amount of items"
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group as={Col} className="mb-3" sm>
+            <FloatingLabel label="Delivery date" >
+              <Form.Control
+                type="date"
+                value={date}
+                onChange={(event) => setDate(event.target.value)}
+                placeholder="Delivery date"
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group as={Col} className="mb-3" sm>
+            <FloatingLabel label="Delivery time (UTC)" >
+              <Form.Control
+                type="time"
+                value={time}
+                onChange={(event) => setTime(event.target.value)}
+                placeholder="Delivery time (UTC)"
+              />
+            </FloatingLabel>
+          </Form.Group>
+        </Row>
+        <Button variant="primary" type="submit" disabled={!fieldsFilled} style={{ marginRight: 10 }}>
+          Calculate
+        </Button>
+        <Button variant="outline-danger" onClick={resetFields}>
+          Reset
+        </Button>
+      </Form>
+    </div>
   );
 };
 
